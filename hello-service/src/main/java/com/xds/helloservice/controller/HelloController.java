d845d13ea8dcd431941b07405adfa22be045cb51
xudongsheng <xudongsheng@dhgate.com>
2018-12-18 16:13:00 +0800

    config

--- hello-service/src/main/java/com/xds/helloservice/controller/HelloController.java
+++ hello-service/src/main/java/com/xds/helloservice/controller/HelloController.java
@@ -1,11 +1,13 @@
 package com.xds.helloservice.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class HelloController {
 
     @Value("${server.port}")
@@ -13,7 +15,7 @@ public class HelloController {
 
 
     @Value("${money}")
-    String money;
+    private String money;
 
     @RequestMapping("/hi")
     public String home(@RequestParam(value = "name", defaultValue = "lucy") String name) {
